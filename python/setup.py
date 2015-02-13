6c95add56bb9527dae9d835272f8f74b6d139ba9
Sergey Dryabzhinsky <sergey@rusoft.ru>
2015-02-13 04:13:28 +0300

    Update description for PIP

--- python/setup.py
+++ python/setup.py
@@ -48,6 +48,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.2',
     ],
 )

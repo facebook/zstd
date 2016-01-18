87f020c11ca34b6416e8c15d59bfb3337416fd94
Yann Collet <yann.collet.73@gmail.com>
2016-01-18 12:03:27 +0100

    changed : ZSTD_LEGACY_SUPPORT set to 0 by default within `zstd_decompress.c` (#113)

--- lib/zstd_decompress.c
+++ lib/zstd_decompress.c
@@ -44,10 +44,10 @@
 
 /*!
 *  LEGACY_SUPPORT :
-*  ZSTD_decompress() can decode older formats (starting from zstd 0.1+)
+*  ZSTD_decompress() can decode older formats (v0.1+) if set to 1
 */
 #ifndef ZSTD_LEGACY_SUPPORT
-#  define ZSTD_LEGACY_SUPPORT 1
+#  define ZSTD_LEGACY_SUPPORT 0
 #endif
 
 

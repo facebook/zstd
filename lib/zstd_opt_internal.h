87d4f3daa41ad92cbf7345f9a056b822ad3c1dbd
inikep <inikep@gmail.com>
2016-03-02 15:56:24 +0100

    priceTable moved to heap

--- lib/zstd_opt_internal.h
+++ lib/zstd_opt_internal.h
@@ -40,23 +40,7 @@
 *  The optimal parser
 *********************************************/
 /*-  Constants  -*/
-#define ZSTD_OPT_NUM    (1<<12)
 #define ZSTD_FREQ_DIV   5
-
-
-typedef struct {
-    U32 off;
-    U32 len;
-} ZSTD_match_t;
-
-typedef struct {
-    U32 price;
-    U32 off;
-    U32 mlen;
-    U32 litlen;
-    U32 rep;
-    U32 rep2;
-} ZSTD_optimal_t;
 
 
 MEM_STATIC void ZSTD_rescaleFreqs(seqStore_t* ssPtr)

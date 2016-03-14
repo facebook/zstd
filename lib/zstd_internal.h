5fdd0b46d319820370bc96d0fb69212d9605cdd9
inikep <inikep@gmail.com>
2016-03-14 19:51:11 +0100

    added additionalParam

--- lib/zstd_internal.h
+++ lib/zstd_internal.h
@@ -226,5 +226,7 @@ typedef struct {
 
 seqStore_t ZSTD_copySeqStore(const ZSTD_CCtx* ctx);
 
+void ZSTD_setAdditionalParam(ZSTD_CCtx* ctx, int additionalParam);
+
 
 #endif   /* ZSTD_CCOMMON_H_MODULE */

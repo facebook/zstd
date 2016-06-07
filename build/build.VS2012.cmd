0faabe43f8d43387a9ab6d005243d8d0f0ea86c8
Christophe Chevalier <chevalier@doxense.com>
2016-06-07 14:00:02 +0200

    Use Rebuild target instead of Clean+Build

--- build/build.VS2012.cmd
+++ build/build.VS2012.cmd
@@ -1,6 +1,6 @@
 @echo off
 
 rem build 32-bit
-call "%~p0%build.generic.cmd" VS2012 Release "Clean,Build" Win32 v110
+call "%~p0%build.generic.cmd" VS2012 Release Rebuild Win32 v110
 rem build 64-bit
-call "%~p0%build.generic.cmd" VS2012 Release "Clean,Build" x64 v110
+call "%~p0%build.generic.cmd" VS2012 Release Rebuild x64 v110

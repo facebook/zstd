6870d9e8e6b9d816fe263a03fc0a1a55a85c1a81
inikep <inikep@gmail.com>
2016-08-02 10:12:42 +0200

    removed Huffman quad-symbol decoding

--- programs/playTests.sh
+++ programs/playTests.sh
@@ -82,9 +82,9 @@ echo "\n**** dictionary tests **** "
 ./datagen -g1M | $ZSTD -D tmpDict | $ZSTD -D tmpDict -dvq | md5sum > tmp2
 diff -q tmp1 tmp2
 $ZSTD --train *.c *.h -o tmpDict
-$ZSTD xxhash.c -D tmpDict -of tmp
+$ZSTD paramgrill.c -D tmpDict -of tmp
 $ZSTD -d tmp -D tmpDict -of result
-diff xxhash.c result
+diff paramgrill.c result
 
 
 echo "\n**** multiple files tests **** "

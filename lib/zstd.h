c56ece8a212e11a410c27ad44cd399e928f411cf
Yann Collet <yann.collet.73@gmail.com>
2016-08-19 18:32:30 +0200

    update version number

--- lib/zstd.h
+++ lib/zstd.h
@@ -55,7 +55,7 @@ extern "C" {
 /*=======   Version   =======*/
 #define ZSTD_VERSION_MAJOR    0
 #define ZSTD_VERSION_MINOR    8
-#define ZSTD_VERSION_RELEASE  1
+#define ZSTD_VERSION_RELEASE  2
 
 #define ZSTD_LIB_VERSION ZSTD_VERSION_MAJOR.ZSTD_VERSION_MINOR.ZSTD_VERSION_RELEASE
 #define ZSTD_QUOTE(str) #str

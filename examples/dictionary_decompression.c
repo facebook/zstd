4ded9e591cbed57c54fc8f7a50412af5980e23a7
Yann Collet <cyan@fb.com>
2016-08-30 10:04:33 -0700

    added boilerplate

--- examples/dictionary_decompression.c
+++ examples/dictionary_decompression.c
@@ -1,27 +1,12 @@
-/*
-  Dictionary decompression
-  Educational program using zstd library
-  Copyright (C) Yann Collet 2016
+/**
+ * Copyright 2016-present, Yann Collet, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE-examples file in the root directory of this source tree.
+ */
 
-  GPL v2 License
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License along
-  with this program; if not, write to the Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-  You can contact the author at :
-  - zstd homepage : http://www.zstd.net/
-*/
 
 #include <stdlib.h>    // malloc, exit
 #include <stdio.h>     // printf

f45c550842476bf47cfea0939674674285fbbd02
Yann Collet <cyan@fb.com>
2016-08-31 09:16:19 -0700

    update brew formula

--- zstd.rb
+++ zstd.rb
@@ -1,8 +1,8 @@
 class Zstd < Formula
   desc "Zstandard - Fast real-time compression algorithm"
   homepage "http://www.zstd.net/"
-  url "https://github.com/Cyan4973/zstd/archive/v0.8.1.tar.gz"
-  sha256 "4632bee45988dd0fe3edf1e67bdf0a833895cbb1a7d1eb23ef0b7d753f8bffdd"
+  url "https://github.com/Cyan4973/zstd/archive/v1.0.0.tar.gz"
+  sha256 "197e6ef74da878cbf72844f38461bb18129d144fd5221b3598e973ecda6f5963"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"

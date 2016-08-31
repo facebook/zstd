9df509447eafd0cdd4cfb1511802858d3e796d55
Yann Collet <cyan@fb.com>
2016-08-31 07:34:18 -0700

    copy zlib license into file header

--- zlibWrapper/examples/example.c
+++ zlibWrapper/examples/example.c
@@ -1,8 +1,25 @@
-/* the file contains minimal changes required to be compiled with zstd wrapper for zlib */
-
 /* example.c -- usage example of the zlib compression library
- * Copyright (C) 1995-2006, 2011 Jean-loup Gailly.
- * For conditions of distribution and use, see copyright notice in zlib.h
+ * the file contains minimal changes required to be compiled with zstd wrapper for zlib
+ */
+
+ /*
+  Copyright (c) 1995-2006, 2011 Jean-loup Gailly
+
+ This software is provided 'as-is', without any express or implied
+ warranty. In no event will the authors be held liable for any damages
+ arising from the use of this software.
+
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+
+ 1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgement in the product documentation would be
+    appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
  */
 
 /* @(#) $Id$ */

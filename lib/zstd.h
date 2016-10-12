6c7a2a1a3e96fb18f287af3465fc6c58c3548b98
Yann Collet <cyan@fb.com>
2016-10-11 17:29:27 -0700

    bumped version number

--- lib/zstd.h
+++ lib/zstd.h
@@ -33,7 +33,7 @@ extern "C" {
 /*=======   Version   =======*/
 #define ZSTD_VERSION_MAJOR    1
 #define ZSTD_VERSION_MINOR    1
-#define ZSTD_VERSION_RELEASE  0
+#define ZSTD_VERSION_RELEASE  1
 
 #define ZSTD_LIB_VERSION ZSTD_VERSION_MAJOR.ZSTD_VERSION_MINOR.ZSTD_VERSION_RELEASE
 #define ZSTD_QUOTE(str) #str

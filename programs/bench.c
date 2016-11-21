5ddcd9d9ae438bc497c39a0c4cf095fcbcfde8ad
Przemyslaw Skibinski <inikep@gmail.com>
2016-11-21 16:37:56 +0100

    bench.c: fixed MAX_CLEVEL

--- programs/bench.c
+++ programs/bench.c
@@ -483,11 +483,17 @@ static void BMK_syntheticTest(int cLevel, int cLevelLast, double compressibility
     free(srcBuffer);
 }
 
+#define ZSTD_MAX_CLEVEL     22
 
 int BMK_benchFiles(const char** fileNamesTable, unsigned nbFiles,
                    const char* dictFileName, int cLevel, int cLevelLast)
 {
     double const compressibility = (double)g_compressibilityDefault / 100;
+
+    if (cLevel > ZSTD_MAX_CLEVEL) cLevel = ZSTD_MAX_CLEVEL;
+    if (cLevelLast > ZSTD_MAX_CLEVEL) cLevelLast = ZSTD_MAX_CLEVEL;
+    if (cLevelLast < cLevel) cLevelLast = cLevel;
+    if (cLevelLast > cLevel) DISPLAYLEVEL(2, "Benchmarking levels from %d to %d\n", cLevel, cLevelLast); 
 
     if (nbFiles == 0)
         BMK_syntheticTest(cLevel, cLevelLast, compressibility);

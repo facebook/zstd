a1f60632cb29b407af70b88670c78109cf412355
Przemyslaw Skibinski <inikep@gmail.com>
2016-11-29 15:50:28 +0100

    gz_statep is union

--- zlibWrapper/gzclose.c
+++ zlibWrapper/gzclose.c
@@ -18,7 +18,7 @@ int ZEXPORT gzclose(file)
         return Z_STREAM_ERROR;
     state = (gz_statep)file;
 
-    return state->mode == GZ_READ ? gzclose_r(file) : gzclose_w(file);
+    return state.state->mode == GZ_READ ? gzclose_r(file) : gzclose_w(file);
 #else
     return gzclose_r(file);
 #endif

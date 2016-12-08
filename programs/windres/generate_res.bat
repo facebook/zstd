c2f141f8e82c57cb1178a8ddb6cbef93792406a4
Przemyslaw Skibinski <inikep@gmail.com>
2016-12-08 10:49:59 +0100

    improved MSYS support

--- programs/windres/generate_res.bat
+++ programs/windres/generate_res.bat
@@ -6,6 +6,6 @@ IF ERRORLEVEL 1 (
     ECHO The windres.exe is missing. Ensure it is installed and placed in your PATH.
     EXIT /B
 ) ELSE (
-    windres.exe -I ..\lib -I windres -i windres\zstd.rc -O coff -F pe-x86-64 -o windres\zstd64.res
-    windres.exe -I ..\lib -I windres -i windres\zstd.rc -O coff -F pe-i386 -o windres\zstd32.res
+    windres.exe -I ../lib -I windres -i windres/zstd.rc -O coff -F pe-x86-64 -o windres/zstd64.res
+    windres.exe -I ../lib -I windres -i windres/zstd.rc -O coff -F pe-i386 -o windres/zstd32.res
 )

1e44aeb41c891d2a2e6e7e10ec5a716386d596ed
Yann Collet <cyan@fb.com>
2017-01-12 01:25:46 +0100

    zstdmt : changed internal naming from frame to chunk
    Since the result of mt compression is a single frame,
    changed naming, which implied the concatenation of multiple frames.
    
    minor : ensures that content size is written in header

--- lib/compress/zstdmt_compress.h
+++ lib/compress/zstdmt_compress.h
@@ -1,12 +1,24 @@
 
+/* ===   Dependencies   === */
 #include <stddef.h>   /* size_t */
+#include "zstd.h"     /* ZSTD_inBuffer, ZSTD_outBuffer */
+
+
+/* ===   Simple one-pass functions   === */
 
 typedef struct ZSTDMT_CCtx_s ZSTDMT_CCtx;
-
-ZSTDMT_CCtx *ZSTDMT_createCCtx(unsigned nbThreads);
+ZSTDMT_CCtx* ZSTDMT_createCCtx(unsigned nbThreads);
 size_t ZSTDMT_freeCCtx(ZSTDMT_CCtx* cctx);
 
 size_t ZSTDMT_compressCCtx(ZSTDMT_CCtx* cctx,
                            void* dst, size_t dstCapacity,
                      const void* src, size_t srcSize,
                            int compressionLevel);
+
+
+/* ===   Streaming functions   === */
+
+size_t ZSTDMT_initCStream(ZSTDMT_CCtx* zcs, int compressionLevel);
+size_t ZSTDMT_compressStream(ZSTDMT_CCtx* zcs, ZSTD_outBuffer* output, ZSTD_inBuffer* input);
+size_t ZSTDMT_flushStream(ZSTDMT_CCtx* zcs, ZSTD_outBuffer* output);
+size_t ZSTDMT_endStream(ZSTDMT_CCtx* zcs, ZSTD_outBuffer* output);

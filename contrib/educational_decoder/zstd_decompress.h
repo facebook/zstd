39037f528f799e44fba367b0dcbe2172a4250032
Sean Purcell <me@seanp.xyz>
2017-01-30 14:42:21 -0800

    Added ZSTD_get_decompressed_size
    Since this implementation handles multiple concatenated frames,
    to determine decompressed size we must traverse the entire input,
    checking each frame's frame_content_size field

--- contrib/educational_decoder/zstd_decompress.h
+++ contrib/educational_decoder/zstd_decompress.h
@@ -3,4 +3,5 @@ size_t ZSTD_decompress(void *dst, size_t dst_len, const void *src,
 size_t ZSTD_decompress_with_dict(void *dst, size_t dst_len, const void *src,
                                  size_t src_len, const void *dict,
                                  size_t dict_len);
+size_t ZSTD_get_decompressed_size(const void *src, size_t src_len);
 

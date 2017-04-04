79298bf187181e9edc37c5b766ae4020d9972d9f
Nick Terrell <terrelln@fb.com>
2017-04-04 11:53:22 -0700

    Add userland test with mock kernel headers

---
+++ contrib/linux-kernel/test/include/linux/kernel.h
@@ -0,0 +1,14 @@
+#ifndef LINUX_KERNEL_H_
+#define LINUX_KERNEL_H_
+
+#define ALIGN(x, a) ({                                                         \
+    typeof(x) const __xe = (x);                                                \
+    typeof(a) const __ae = (a);                                                \
+    typeof(a) const __m = __ae - 1;                                            \
+    typeof(x) const __r = __xe & __m;                                          \
+    __xe + (__r ? (__ae - __r) : 0);                                           \
+  })
+
+#define PTR_ALIGN(p, a) (typeof(p))ALIGN((unsigned long long)(p), (a))
+
+#endif // LINUX_KERNEL_H_

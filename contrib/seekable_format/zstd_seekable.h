2785b28e05d715a99fa66beee12bf47422fb8601
Sean Purcell <me@seanp.xyz>
2017-04-12 14:09:13 -0700

    Reduce the limit on frame decompressed size to 2 GB

--- contrib/seekable_format/zstd_seekable.h
+++ contrib/seekable_format/zstd_seekable.h
@@ -11,8 +11,8 @@ static const unsigned ZSTD_seekTableFooterSize = 9;
 
 #define ZSTD_SEEKABLE_MAXFRAMES 0x8000000U
 
-/* 0xFE03F607 is the largest number x such that ZSTD_compressBound(x) fits in a 32-bit integer */
-#define ZSTD_SEEKABLE_MAX_FRAME_DECOMPRESSED_SIZE 0xFE03F607
+/* Limit the maximum size to avoid any potential issues storing the compressed size */
+#define ZSTD_SEEKABLE_MAX_FRAME_DECOMPRESSED_SIZE 0x80000000U
 
 /*-****************************************************************************
 *  Seekable Format

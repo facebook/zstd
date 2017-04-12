db5af1e8bdab64a65106861f3281cbe43e1d0065
Sean Purcell <me@seanp.xyz>
2017-04-12 11:06:00 -0700

    s/chunk/frame/

--- contrib/seekable_format/zstd_seekable_compression_format.md
+++ contrib/seekable_format/zstd_seekable_compression_format.md
@@ -18,7 +18,7 @@ Distribution of this document is unlimited.
 
 ## Introduction
 This document defines a format for compressed data to be stored so that subranges of the data can be efficiently decompressed without requiring the entire document to be decompressed.
-This is done by splitting up the input data into chunks,
+This is done by splitting up the input data into frames,
 each of which are compressed independently,
 and so can be decompressed independently.
 Decompression then takes advantage of a provided 'seek table', which allows the decompressor to immediately jump to the desired data.  This is done in a way that is compatible with the original Zstandard format by placing the seek table in a Zstandard skippable frame.
@@ -31,7 +31,7 @@ In this document:
 
 ## Format
 
-The format consists of a number of chunks (Zstandard compressed frames and skippable frames), followed by a final skippable frame at the end containing the seek table.
+The format consists of a number of frames (Zstandard compressed frames and skippable frames), followed by a final skippable frame at the end containing the seek table.
 
 ### Seek Table Format
 The structure of the seek table frame is as follows:
@@ -58,7 +58,7 @@ This is for compatibility with [Zstandard skippable frames].
 #### `Seek_Table_Footer`
 The seek table footer format is as follows:
 
-|`Number_Of_Chunks`|`Seek_Table_Descriptor`|`Seekable_Magic_Number`|
+|`Number_Of_Frames`|`Seek_Table_Descriptor`|`Seekable_Magic_Number`|
 |------------------|-----------------------|-----------------------|
 | 4 bytes          | 1 byte                | 4 bytes               |
 
@@ -68,9 +68,9 @@ Value : 0x8F92EAB1.
 This value must be the last bytes present in the compressed file so that decoders
 can efficiently find it and determine if there is an actual seek table present.
 
-__`Number_Of_Chunks`__
+__`Number_Of_Frames`__
 
-The number of stored chunks in the data.
+The number of stored frames in the data.
 
 __`Seek_Table_Descriptor`__
 
@@ -87,13 +87,13 @@ for example the addition of inline dictionaries.
 
 __`Checksum_Flag`__
 
-If the checksum flag is set, each of the seek table entries contains a 4 byte checksum of the uncompressed data contained in its chunk.
+If the checksum flag is set, each of the seek table entries contains a 4 byte checksum of the uncompressed data contained in its frame.
 
 `Reserved_Bits` are not currently used but may be used in the future for breaking changes, so a compliant decoder should ensure they are set to 0.  `Unused_Bits` may be used in the future for non-breaking changes, so a compliant decoder should not interpret these bits.
 
 #### __`Seek_Table_Entries`__
 
-`Seek_Table_Entries` consists of `Number_Of_Chunks` (one for each chunk in the data, not including the seek table frame) entries of the following form, in sequence:
+`Seek_Table_Entries` consists of `Number_Of_Frames` (one for each frame in the data, not including the seek table frame) entries of the following form, in sequence:
 
 |`Compressed_Size`|`Decompressed_Size`|`[Checksum]`|
 |-----------------|-------------------|------------|
@@ -101,12 +101,12 @@ If the checksum flag is set, each of the seek table entries contains a 4 byte ch
 
 __`Compressed_Size`__
 
-The compressed size of the chunk.
-The cumulative sum of the `Compressed_Size` fields of chunks `0` to `i` gives the offset in the compressed file of chunk `i+1`.
+The compressed size of the frame.
+The cumulative sum of the `Compressed_Size` fields of frames `0` to `i` gives the offset in the compressed file of frame `i+1`.
 
 __`Decompressed_Size`__
 
-The size of the decompressed data contained in the chunk.  For skippable or otherwise empty frames, this value is 0.
+The size of the decompressed data contained in the frame.  For skippable or otherwise empty frames, this value is 0.
 
 __`Checksum`__
 
--- contrib/seekable_format/zstdseek_decompress.c
+++ contrib/seekable_format/zstdseek_decompress.c
@@ -29,11 +29,11 @@ typedef struct {
     int checksumFlag;
 } seekTable_t;
 
-/** ZSTD_seekable_offsetToChunk() :
- *  Performs a binary search to find the last chunk with a decompressed offset
+/** ZSTD_seekable_offsetToFrame() :
+ *  Performs a binary search to find the last frame with a decompressed offset
  *  <= pos
- *  @return : the chunk's index */
-static U32 ZSTD_seekable_offsetToChunk(const seekTable_t* table, U64 pos)
+ *  @return : the frame's index */
+static U32 ZSTD_seekable_offsetToFrame(const seekTable_t* table, U64 pos)
 {
     U32 lo = 0;
     U32 hi = table->tableLen;
@@ -61,7 +61,7 @@ struct ZSTD_seekable_DStream_s {
     ZSTD_DStream* dstream;
     seekTable_t seekTable;
 
-    U32 curChunk;
+    U32 curFrame;
     U64 compressedOffset;
     U64 decompressedOffset;
 
@@ -107,7 +107,7 @@ size_t ZSTD_seekable_loadSeekTable(ZSTD_seekable_DStream* zds, const void* src, 
 {
     const BYTE* ip = (const BYTE*)src + srcSize;
 
-    U32 numChunks;
+    U32 numFrames;
     int checksumFlag;
 
     U32 sizePerEntry;
@@ -129,10 +129,10 @@ size_t ZSTD_seekable_loadSeekTable(ZSTD_seekable_DStream* zds, const void* src, 
         }
     }
 
-    numChunks = MEM_readLE32(ip-9);
+    numFrames = MEM_readLE32(ip-9);
     sizePerEntry = 8 + (checksumFlag?4:0);
 
-    {   U32 const tableSize = sizePerEntry * numChunks;
+    {   U32 const tableSize = sizePerEntry * numFrames;
         U32 const frameSize = tableSize + ZSTD_seekTableFooterSize + ZSTD_skippableHeaderSize;
 
         const BYTE* base = ip - frameSize;
@@ -148,7 +148,7 @@ size_t ZSTD_seekable_loadSeekTable(ZSTD_seekable_DStream* zds, const void* src, 
 
         {   /* Allocate an extra entry at the end so that we can do size
              * computations on the last element without special case */
-            seekEntry_t* entries = malloc(sizeof(seekEntry_t) * (numChunks + 1));
+            seekEntry_t* entries = malloc(sizeof(seekEntry_t) * (numFrames + 1));
             const BYTE* tableBase = base + ZSTD_skippableHeaderSize;
 
             U32 idx;
@@ -163,7 +163,7 @@ size_t ZSTD_seekable_loadSeekTable(ZSTD_seekable_DStream* zds, const void* src, 
             }
 
             /* compute cumulative positions */
-            for (idx = 0, pos = 0; idx < numChunks; idx++) {
+            for (idx = 0, pos = 0; idx < numFrames; idx++) {
                 entries[idx].cOffset = cOffset;
                 entries[idx].dOffset = dOffset;
 
@@ -174,11 +174,11 @@ size_t ZSTD_seekable_loadSeekTable(ZSTD_seekable_DStream* zds, const void* src, 
                     pos += 4;
                 }
             }
-            entries[numChunks].cOffset = cOffset;
-            entries[numChunks].dOffset = dOffset;
+            entries[numFrames].cOffset = cOffset;
+            entries[numFrames].dOffset = dOffset;
 
             zds->seekTable.entries = entries;
-            zds->seekTable.tableLen = numChunks;
+            zds->seekTable.tableLen = numFrames;
             zds->seekTable.checksumFlag = checksumFlag;
             return 0;
         }
@@ -197,7 +197,7 @@ size_t ZSTD_seekable_initDStream(ZSTD_seekable_DStream* zds, U64 rangeStart, U64
     zds->stage = zsds_seek;
 
     /* force a seek first */
-    zds->curChunk = (U32) -1;
+    zds->curFrame = (U32) -1;
     zds->compressedOffset = (U64) -1;
     zds->decompressedOffset = (U64) -1;
 
@@ -271,7 +271,7 @@ size_t ZSTD_seekable_decompressStream(ZSTD_seekable_DStream* zds, ZSTD_outBuffer
                     /* need more input */
                     return MIN(
                             ZSTD_DStreamInSize(),
-                            (size_t)(jt->entries[zds->curChunk + 1]
+                            (size_t)(jt->entries[zds->curFrame + 1]
                                              .cOffset -
                                      zds->compressedOffset));
                 }
@@ -281,7 +281,7 @@ size_t ZSTD_seekable_decompressStream(ZSTD_seekable_DStream* zds, ZSTD_outBuffer
             {
                 U64 const toDecompress =
                         MIN(zds->targetEnd,
-                            jt->entries[zds->curChunk + 1].dOffset) -
+                            jt->entries[zds->curFrame + 1].dOffset) -
                         zds->decompressedOffset;
                 size_t const prevInputPos = input->pos;
 
@@ -305,7 +305,7 @@ size_t ZSTD_seekable_decompressStream(ZSTD_seekable_DStream* zds, ZSTD_outBuffer
                     if (ret == 0) {
                         /* verify the checksum */
                         U32 const digest = XXH64_digest(&zds->xxhState) & 0xFFFFFFFFU;
-                        if (digest != jt->entries[zds->curChunk].checksum) {
+                        if (digest != jt->entries[zds->curFrame].checksum) {
                             return ERROR(checksum_wrong);
                         }
 
@@ -323,9 +323,9 @@ size_t ZSTD_seekable_decompressStream(ZSTD_seekable_DStream* zds, ZSTD_outBuffer
                     /* frame is done */
                     /* make sure this lines up with the expected frame border */
                     if (zds->decompressedOffset !=
-                                jt->entries[zds->curChunk + 1].dOffset ||
+                                jt->entries[zds->curFrame + 1].dOffset ||
                         zds->compressedOffset !=
-                                jt->entries[zds->curChunk + 1].cOffset)
+                                jt->entries[zds->curFrame + 1].cOffset)
                         return ERROR(corruption_detected);
                     ZSTD_resetDStream(zds->dstream);
                     zds->stage = zsds_seek;
@@ -334,29 +334,29 @@ size_t ZSTD_seekable_decompressStream(ZSTD_seekable_DStream* zds, ZSTD_outBuffer
 
                 /* need more input */
                 return MIN(ZSTD_DStreamInSize(), (size_t)(
-                        jt->entries[zds->curChunk + 1].cOffset -
+                        jt->entries[zds->curFrame + 1].cOffset -
                         zds->compressedOffset));
             }
         }
         case zsds_seek: {
-            U32 targetChunk;
+            U32 targetFrame;
             if (zds->decompressedOffset < zds->targetStart ||
                     zds->decompressedOffset >= zds->targetEnd) {
                 /* haven't started yet */
-                targetChunk = ZSTD_seekable_offsetToChunk(jt, zds->targetStart);
+                targetFrame = ZSTD_seekable_offsetToFrame(jt, zds->targetStart);
             } else {
-                targetChunk = ZSTD_seekable_offsetToChunk(jt, zds->decompressedOffset);
+                targetFrame = ZSTD_seekable_offsetToFrame(jt, zds->decompressedOffset);
             }
 
-            zds->curChunk = targetChunk;
+            zds->curFrame = targetFrame;
 
-            if (zds->compressedOffset == jt->entries[targetChunk].cOffset) {
+            if (zds->compressedOffset == jt->entries[targetFrame].cOffset) {
                 zds->stage = zsds_decompress;
                 break;
             }
 
-            zds->nextSeek = jt->entries[targetChunk].cOffset;
-            zds->decompressedOffset = jt->entries[targetChunk].dOffset;
+            zds->nextSeek = jt->entries[targetFrame].cOffset;
+            zds->decompressedOffset = jt->entries[targetFrame].dOffset;
             /* signal to user that a seek is required */
             return ERROR(needSeek);
         }

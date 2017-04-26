0075a1bae88521e5b4a2e1f2da1d807f5afd1106
niXman <i.nixman@autistici.org>
2017-04-26 13:04:04 +0300

    unchecked argv[idx] access

--- examples/simple_compression.c
+++ examples/simple_compression.c
@@ -116,7 +116,6 @@ static char* createOutFilename_orDie(const char* filename)
 int main(int argc, const char** argv)
 {
     const char* const exeName = argv[0];
-    const char* const inFilename = argv[1];
 
     if (argc!=2) {
         printf("wrong arguments\n");
@@ -124,6 +123,8 @@ int main(int argc, const char** argv)
         printf("%s FILE\n", exeName);
         return 1;
     }
+
+    const char* const inFilename = argv[1];
 
     char* const outFilename = createOutFilename_orDie(inFilename);
     compress_orDie(inFilename, outFilename);

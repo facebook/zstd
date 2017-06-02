c4a5a21c5c4e75b24839267a4f0472948a0159cf
Yann Collet <cyan@fb.com>
2017-06-01 17:56:14 -0700

    created ZSTDMT_sizeof_CCtx() and POOL_sizeof()
    required by ZSTD_sizeofCCtx() while adding a ZSTDMT_CCtx*

--- lib/common/pool.c
+++ lib/common/pool.c
@@ -146,6 +146,13 @@ void POOL_free(POOL_ctx *ctx) {
     free(ctx);
 }
 
+size_t POOL_sizeof(POOL_ctx *ctx) {
+    if (ctx==NULL) return 0;  /* supports sizeof NULL */
+    return sizeof(*ctx)
+        + ctx->queueSize * sizeof(POOL_job)
+        + ctx->numThreads * sizeof(pthread_t);
+}
+
 void POOL_add(void *ctxVoid, POOL_function function, void *opaque) {
     POOL_ctx *ctx = (POOL_ctx *)ctxVoid;
     if (!ctx) { return; }

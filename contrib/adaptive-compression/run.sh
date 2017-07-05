40e7fbafcb88adace07b08a7ddcb4f6e5295bbd5
Paul Cruz <paulcruz74@fb.com>
2017-07-05 11:52:55 -0700

    removed calculation of file size and replaced with limited number of available jobs

--- contrib/adaptive-compression/run.sh
+++ contrib/adaptive-compression/run.sh
@@ -1,6 +1,39 @@
 make clean multi
+
 ./multi tests/test2048.pdf tmp.zst
 zstd -d tmp.zst
 diff tmp tests/test2048.pdf
 echo "diff test complete"
+rm tmp*
+
+./multi tests/test512.pdf tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test512.pdf
+echo "diff test complete"
+rm tmp*
+
+./multi tests/test64.pdf tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test64.pdf
+echo "diff test complete"
+rm tmp*
+
+./multi tests/test16.pdf tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test16.pdf
+echo "diff test complete"
+rm tmp*
+
+./multi tests/test4.pdf tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test4.pdf
+echo "diff test complete"
+rm tmp*
+
+./multi tests/test.pdf tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test.pdf
+echo "diff test complete"
+rm tmp*
+
 make clean

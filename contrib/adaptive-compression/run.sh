6a3a85c12b78ac46a400d579cb5c95876138363c
Paul Cruz <paulcruz74@fb.com>
2017-07-05 09:57:50 -0700

    renamed files

--- contrib/adaptive-compression/run.sh
+++ contrib/adaptive-compression/run.sh
@@ -1,2 +1,6 @@
-make clean v2
-./v2 tests/test2048.pdf tmp.zst
+make clean multi
+./multi tests/test2048.pdf tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test2048.pdf
+echo "diff test complete"
+make clean

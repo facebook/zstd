f7e6b358d04d8b1da2599ed039c1c12d301db6f2
Paul Cruz <paulcruz74@fb.com>
2017-07-07 10:29:06 -0700

    added tests that check to ensure stdout is working

--- contrib/adaptive-compression/run.sh
+++ contrib/adaptive-compression/run.sh
@@ -76,6 +76,42 @@ diff tmp tests/test.pdf
 echo "diff test complete: test.pdf"
 rm tmp*
 
+cat tests/test2048.pdf | ./multi > tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test2048.pdf
+echo "diff test complete: test2048.pdf"
+rm tmp*
+
+cat tests/test512.pdf | ./multi > tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test512.pdf
+echo "diff test complete: test512.pdf"
+rm tmp*
+
+cat tests/test64.pdf | ./multi > tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test64.pdf
+echo "diff test complete: test64.pdf"
+rm tmp*
+
+cat tests/test16.pdf | ./multi > tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test16.pdf
+echo "diff test complete: test16.pdf"
+rm tmp*
+
+cat tests/test4.pdf | ./multi > tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test4.pdf
+echo "diff test complete: test4.pdf"
+rm tmp*
+
+cat tests/test.pdf | ./multi > tmp.zst
+zstd -d tmp.zst
+diff tmp tests/test.pdf
+echo "diff test complete: test.pdf"
+rm tmp*
+
 echo "Running multi-file tests"
 ./multi tests/*
 zstd -d tests/test.pdf.zst -o tests/tmp

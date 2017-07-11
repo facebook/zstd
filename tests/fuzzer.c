ca937dfa01e70f9d11a5f5281ac2c58ff50f55af
Yann Collet <cyan@fb.com>
2017-07-11 08:54:29 -0700

    zstdmt: removed margin for improved memory usage

--- tests/fuzzer.c
+++ tests/fuzzer.c
@@ -118,6 +118,8 @@ static void* FUZ_mallocDebug(void* counter, size_t size)
     mallocCounter_t* const mcPtr = (mallocCounter_t*)counter;
     void* const ptr = malloc(size);
     if (ptr==NULL) return NULL;
+    DISPLAYLEVEL(4, "allocating %u KB => effectively %u KB \n",
+        (U32)(size >> 10), (U32)(malloc_size(ptr) >> 10));  /* OS-X specific */
     mcPtr->totalMalloc += size;
     mcPtr->currentMalloc += size;
     if (mcPtr->currentMalloc > mcPtr->peakMalloc)

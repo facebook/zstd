e2de6c56175bfb9e3a1a7fd81422776456a5b294
Paul Cruz <paulcruz74@fb.com>
2017-07-14 16:29:29 -0700

    updated tests to use different seeds when executing different tests

--- contrib/adaptive-compression/test-correctness.sh
+++ contrib/adaptive-compression/test-correctness.sh
@@ -1,238 +1,238 @@
 echo "correctness tests -- general"
-./datagen -g1GB > tmp
+./datagen -s1 -g1GB > tmp
 ./adapt -otmp.zst tmp
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g500MB > tmp
+./datagen -s2 -g500MB > tmp
 ./adapt -otmp.zst tmp
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g250MB > tmp
+./datagen -s3 -g250MB > tmp
 ./adapt -otmp.zst tmp
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g125MB > tmp
+./datagen -s4 -g125MB > tmp
 ./adapt -otmp.zst tmp
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g50MB > tmp
+./datagen -s5 -g50MB > tmp
 ./adapt -otmp.zst tmp
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g25MB > tmp
+./datagen -s6 -g25MB > tmp
 ./adapt -otmp.zst tmp
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g10MB > tmp
+./datagen -s7 -g10MB > tmp
 ./adapt -otmp.zst tmp
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g5MB > tmp
+./datagen -s8 -g5MB > tmp
 ./adapt -otmp.zst tmp
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g500KB > tmp
+./datagen -s9 -g500KB > tmp
 ./adapt -otmp.zst tmp
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
 echo -e "\ncorrectness tests -- streaming"
-./datagen -g1GB > tmp
+./datagen -s10 -g1GB > tmp
 cat tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g100MB > tmp
+./datagen -s11 -g100MB > tmp
 cat tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g10MB > tmp
+./datagen -s12 -g10MB > tmp
 cat tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g1MB > tmp
+./datagen -s13 -g1MB > tmp
 cat tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g100KB > tmp
+./datagen -s14 -g100KB > tmp
 cat tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g10KB > tmp
+./datagen -s15 -g10KB > tmp
 cat tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
 echo -e "\ncorrectness tests -- read limit"
-./datagen -g1GB > tmp
+./datagen -s16 -g1GB > tmp
 pv -L 50m -q tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g100MB > tmp
+./datagen -s17 -g100MB > tmp
 pv -L 50m -q tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g10MB > tmp
+./datagen -s18 -g10MB > tmp
 pv -L 50m -q tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g1MB > tmp
+./datagen -s19 -g1MB > tmp
 pv -L 50m -q tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g100KB > tmp
+./datagen -s20 -g100KB > tmp
 pv -L 50m -q tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g10KB > tmp
+./datagen -s21 -g10KB > tmp
 pv -L 50m -q tmp | ./adapt > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
 echo -e "\ncorrectness tests -- write limit"
-./datagen -g1GB > tmp
+./datagen -s22 -g1GB > tmp
 pv -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g100MB > tmp
+./datagen -s23 -g100MB > tmp
 pv -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g10MB > tmp
+./datagen -s24 -g10MB > tmp
 pv -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g1MB > tmp
+./datagen -s25 -g1MB > tmp
 pv -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g100KB > tmp
+./datagen -s26 -g100KB > tmp
 pv -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g10KB > tmp
+./datagen -s27 -g10KB > tmp
 pv -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
 echo -e "\ncorrectness tests -- read and write limits"
-./datagen -g1GB > tmp
+./datagen -s28 -g1GB > tmp
 pv -L 50m -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g100MB > tmp
+./datagen -s29 -g100MB > tmp
 pv -L 50m -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g10MB > tmp
+./datagen -s30 -g10MB > tmp
 pv -L 50m -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g1MB > tmp
+./datagen -s31 -g1MB > tmp
 pv -L 50m -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g100KB > tmp
+./datagen -s32 -g100KB > tmp
 pv -L 50m -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
-./datagen -g10KB > tmp
+./datagen -s33 -g10KB > tmp
 pv -L 50m -q tmp | ./adapt | pv -L 5m -q > tmp.zst
 zstd -d tmp.zst -o tmp2
 diff -q tmp tmp2
 rm tmp*
 
 echo -e "\ncorrectness tests -- forced compression level"
-./datagen -g1GB > tmp
+./datagen -s34 -g1GB > tmp
 ./adapt tmp -otmp.zst -i11 -f
 zstd -d tmp.zst -o tmp2
 diff tmp tmp2
 rm tmp*
 
-./datagen -g100MB > tmp
+./datagen -s35 -g100MB > tmp
 ./adapt tmp -otmp.zst -i11 -f
 zstd -d tmp.zst -o tmp2
 diff tmp tmp2
 rm tmp*
 
-./datagen -g10MB > tmp
+./datagen -s36 -g10MB > tmp
 ./adapt tmp -otmp.zst -i11 -f
 zstd -d tmp.zst -o tmp2
 diff tmp tmp2
 rm tmp*
 
-./datagen -g1MB > tmp
+./datagen -s37 -g1MB > tmp
 ./adapt tmp -otmp.zst -i11 -f
 zstd -d tmp.zst -o tmp2
 diff tmp tmp2
 rm tmp*
 
-./datagen -g100KB > tmp
+./datagen -s38 -g100KB > tmp
 ./adapt tmp -otmp.zst -i11 -f
 zstd -d tmp.zst -o tmp2
 diff tmp tmp2
 rm tmp*
 
-./datagen -g10KB > tmp
+./datagen -s39 -g10KB > tmp
 ./adapt tmp -otmp.zst -i11 -f
 zstd -d tmp.zst -o tmp2
 diff tmp tmp2
--- contrib/adaptive-compression/test-performance.sh
+++ contrib/adaptive-compression/test-performance.sh
@@ -1,40 +1,40 @@
 echo "testing time -- no limits set"
-./datagen -g1GB > tmp
+./datagen -s1 -g1GB > tmp
 time ./adapt -otmp1.zst tmp
 time zstd -1 -o tmp2.zst tmp
 rm tmp*
 
-./datagen -g2GB > tmp
+./datagen -s2 -g2GB > tmp
 time ./adapt -otmp1.zst tmp
 time zstd -1 -o tmp2.zst tmp
 rm tmp*
 
-./datagen -g4GB > tmp
+./datagen -s3 -g4GB > tmp
 time ./adapt -otmp1.zst tmp
 time zstd -1 -o tmp2.zst tmp
 rm tmp*
 
 echo -e "\ntesting compression ratio -- no limits set"
-./datagen -g1GB > tmp
+./datagen -s4 -g1GB > tmp
 time ./adapt -otmp1.zst tmp
 time zstd -1 -o tmp2.zst tmp
 ls -l tmp1.zst tmp2.zst
 rm tmp*
 
-./datagen -g2GB > tmp
+./datagen -s5 -g2GB > tmp
 time ./adapt -otmp1.zst tmp
 time zstd -1 -o tmp2.zst tmp
 ls -l tmp1.zst tmp2.zst
 rm tmp*
 
-./datagen -g4GB > tmp
+./datagen -s6 -g4GB > tmp
 time ./adapt -otmp1.zst tmp
 time zstd -1 -o tmp2.zst tmp
 ls -l tmp1.zst tmp2.zst
 rm tmp*
 
 echo e "\ntesting performance at various compression levels -- no limits set"
-./datagen -g1GB > tmp
+./datagen -s7 -g1GB > tmp
 echo "adapt"
 time ./adapt -i5 -f tmp -otmp1.zst
 echo "zstdcli"
@@ -42,7 +42,7 @@ time zstd -5 tmp -o tmp2.zst
 ls -l tmp1.zst tmp2.zst
 rm tmp*
 
-./datagen -g1GB > tmp
+./datagen -s8 -g1GB > tmp
 echo "adapt"
 time ./adapt -i10 -f tmp -otmp1.zst
 echo "zstdcli"
@@ -50,7 +50,7 @@ time zstd -10 tmp -o tmp2.zst
 ls -l tmp1.zst tmp2.zst
 rm tmp*
 
-./datagen -g1GB > tmp
+./datagen -s9 -g1GB > tmp
 echo "adapt"
 time ./adapt -i15 -f tmp -otmp1.zst
 echo "zstdcli"

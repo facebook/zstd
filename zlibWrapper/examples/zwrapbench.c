43655f595e9ed7cf8a10c3b21dd72e103c055643
Yann Collet <cyan@fb.com>
2017-08-21 11:24:32 -0700

    fixed a few headers that were too hastily copy/pasted during last license change

--- zlibWrapper/examples/zwrapbench.c
+++ zlibWrapper/examples/zwrapbench.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-present, Yann Collet, Facebook, Inc.
+ * Copyright (c) 2016-present, Przemyslaw Skibinski, Yann Collet, Facebook, Inc.
  * All rights reserved.
  *
  * This source code is licensed under both the BSD-style license (found in the
@@ -34,7 +34,6 @@
 #ifndef ZSTDCLI_CLEVEL_DEFAULT
 #  define ZSTDCLI_CLEVEL_DEFAULT 3
 #endif
-
 
 
 /*-************************************

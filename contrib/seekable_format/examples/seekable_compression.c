91b40ccdd0a4a8757cb54f3159445342bd3b02a5
Yann Collet <cyan@fb.com>
2017-08-29 09:24:11 -0700

    changed license for examples
    intentionnally this time

--- contrib/seekable_format/examples/seekable_compression.c
+++ contrib/seekable_format/examples/seekable_compression.c
@@ -1,9 +1,10 @@
-/**
- * Copyright 2017-present, Facebook, Inc.
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
  * All rights reserved.
  *
- * This source code is licensed under the license found in the
- * LICENSE-examples file in the root directory of this source tree.
+ * This source code is licensed under both the BSD-style license (found in the
+ * LICENSE file in the root directory of this source tree) and the GPLv2 (found
+ * in the COPYING file in the root directory of this source tree).
  */
 
 #include <stdlib.h>    // malloc, free, exit, atoi

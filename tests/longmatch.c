8fa081e13e2d9b286b4505d3a19a9885d5b00ed5
Yann Collet <cyan@fb.com>
2017-09-08 00:09:23 -0700

    updated license header
    to clarify dual-license meaning as "or"

--- tests/longmatch.c
+++ tests/longmatch.c
@@ -1,10 +1,11 @@
 /*
- * Copyright (c) 2016-present, Yann Collet, Facebook, Inc.
+ * Copyright (c) 2017-present, Yann Collet, Facebook, Inc.
  * All rights reserved.
  *
  * This source code is licensed under both the BSD-style license (found in the
  * LICENSE file in the root directory of this source tree) and the GPLv2 (found
  * in the COPYING file in the root directory of this source tree).
+ * You may select, at your option, one of the above-listed licenses.
  */
 
 

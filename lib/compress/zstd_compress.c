884912a8dff00b5f590dce0da4092836b6467c5f
W. Felix Handte <w@felixhandte.com>
2018-05-16 02:31:30 -0400

    Attach Dict When Using ZSTD_lazy Strategies

--- lib/compress/zstd_compress.c
+++ lib/compress/zstd_compress.c
@@ -1244,7 +1244,7 @@ static size_t ZSTD_resetCCtx_usingCDict(ZSTD_CCtx* cctx,
                           || pledgedSrcSize == ZSTD_CONTENTSIZE_UNKNOWN )
                         && !params.forceWindow /* dictMatchState isn't correctly
                                                 * handled in _enforceMaxDist */
-                        && cdict->cParams.strategy <= ZSTD_dfast
+                        && cdict->cParams.strategy <= ZSTD_btlazy2
                         && ZSTD_equivalentCParams(cctx->appliedParams.cParams,
                                                   cdict->cParams);
 

7f9603758ae667ff60f38c228ec18dd1d17278fe
W. Felix Handte <w@felixhandte.com>
2018-06-15 14:08:58 -0400

    Make Sure Position 0 Gets Into the Tree

--- lib/compress/zstd_compress.c
+++ lib/compress/zstd_compress.c
@@ -966,6 +966,7 @@ static void ZSTD_invalidateMatchState(ZSTD_matchState_t* ms)
     ZSTD_window_clear(&ms->window);
 
     ms->nextToUpdate = ms->window.dictLimit + 1;
+    ms->nextToUpdate3 = ms->window.dictLimit + 1;
     ms->loadedDictEnd = 0;
     ms->opt.litLengthSum = 0;  /* force reset of btopt stats */
     ms->dictMatchState = NULL;

818e72b4d5377cb9d9b18106f77bfe2a73268a89
Yann Collet <cyan@fb.com>
2018-06-21 14:58:59 -0700

    added extended POOL test
    abrupt end + downsizing with running jobs remaining in queue.
    
    also : POOL_resize() requires numThreads >= 1

--- lib/common/pool.h
+++ lib/common/pool.h
@@ -45,6 +45,7 @@ void POOL_free(POOL_ctx* ctx);
  *    note : new pool context might have same address as original one, but it's not guaranteed.
  *           consider starting context as consumed, only rely on returned one.
  *    note 2 : only numThreads can be resized, queueSize is unchanged.
+ *    note 3 : `numThreads` must be at least 1
  */
 POOL_ctx* POOL_resize(POOL_ctx* ctx, size_t numThreads);
 

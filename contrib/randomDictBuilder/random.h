1cbcd21175b496aa2b0c41e938b4207940c75837
Jennifer Liu <jenniferliu620@fb.com>
2018-07-16 16:03:04 -0700

    Refactor RANDOM

--- contrib/randomDictBuilder/random.h
+++ contrib/randomDictBuilder/random.h
@@ -8,18 +8,6 @@
 #endif
 #include "zdict.h"
 
-
-/**************************************
-* Context
-***************************************/
-typedef struct {
-  const BYTE *samples;
-  size_t *offsets;
-  const size_t *samplesSizes;
-  size_t nbSamples;
-  U32 totalSamplesSize;
-} RANDOM_ctx_t;
-
 /**
  * A segment is an inclusive range in the source.
  */
@@ -35,19 +23,8 @@ typedef struct {
 } ZDICT_random_params_t;
 
 
-typedef struct {
-    U64 totalSizeToLoad;
-    unsigned oneSampleTooLarge;
-    unsigned nbSamples;
-} fileStats;
-
 
 ZDICTLIB_API size_t ZDICT_trainFromBuffer_random(
     void *dictBuffer, size_t dictBufferCapacity,
     const void *samplesBuffer, const size_t *samplesSizes, unsigned nbSamples,
     ZDICT_random_params_t parameters);
-
-
-int RANDOM_trainFromFiles(const char* dictFileName, unsigned maxDictSize,
-                        const char** fileNamesTable, unsigned nbFiles,
-                        size_t chunkSize, ZDICT_random_params_t *params);

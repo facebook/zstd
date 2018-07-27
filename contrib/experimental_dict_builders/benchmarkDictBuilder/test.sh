fcefe345481a4b72bdbdcb60e837a9d482d84cc4
Jennifer Liu <jenniferliu620@fb.com>
2018-07-27 16:51:38 -0700

    Save segmentFreqs in ctx instead of malloc and memset in SelectSegment

--- contrib/experimental_dict_builders/benchmarkDictBuilder/test.sh
+++ contrib/experimental_dict_builders/benchmarkDictBuilder/test.sh
@@ -1,2 +1,8 @@
-echo "Benchmark with in=../../lib/common"
-./benchmark in=../../../lib/common
+echo "-----------------github--------------------"
+./benchmark in=github
+echo "-----------------hg-commands--------------------"
+./benchmark in=hg-commands
+echo "-----------------hg-changelog--------------------"
+./benchmark in=hg-changelog
+echo "------------------hg-manifest-------------------"
+./benchmark in=hg-manifest

19b605fc3f302fd23313457b72b04d8935b1fb71
Yann Collet <cyan@fb.com>
2018-10-24 16:50:03 -0700

    added comment on public ddict functions

--- lib/decompress/zstd_ddict.h
+++ lib/decompress/zstd_ddict.h
@@ -16,17 +16,29 @@
  *  Dependencies
  *********************************************************/
 #include <stddef.h>   /* size_t */
-#include "zstd.h"     /* ZSTD_DDict */
+#include "zstd.h"     /* ZSTD_DDict, and several public functions */
 
 
 /*-*******************************************************
  *  Interface
  *********************************************************/
 
+/* note: several prototypes are already published in `zstd.h` :
+ * ZSTD_createDDict()
+ * ZSTD_createDDict_byReference()
+ * ZSTD_createDDict_advanced()
+ * ZSTD_freeDDict()
+ * ZSTD_initStaticDDict()
+ * ZSTD_sizeof_DDict()
+ * ZSTD_estimateDDictSize()
+ * ZSTD_getDictID_fromDict()
+ */
+
 const void* ZSTD_DDict_dictContent(const ZSTD_DDict* ddict);
 size_t ZSTD_DDict_dictSize(const ZSTD_DDict* ddict);
 
 void ZSTD_copyDDictParameters(ZSTD_DCtx* dctx, const ZSTD_DDict* ddict);
+
 
 
 #endif /* ZSTD_DDICT_H */

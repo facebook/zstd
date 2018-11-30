9183c9a60acf8ba6a24cc1354accf4c8b6b8a3aa
Lzu Tao <taolzu@gmail.com>
2018-11-30 11:01:19 +0700

    Change tabsize

--- build/meson/GetZstdLibraryVersion.py
+++ build/meson/GetZstdLibraryVersion.py
@@ -12,34 +12,34 @@ import sys
 
 
 def usage():
-    print('usage: python3 GetZstdLibraryVersion.py <path/to/zstd.h>')
-    sys.exit(1)
+  print('usage: python3 GetZstdLibraryVersion.py <path/to/zstd.h>')
+  sys.exit(1)
 
 
 def find_version(filepath):
-    version_file_data = None
-    with open(filepath) as fd:
-        version_file_data = fd.read()
+  version_file_data = None
+  with open(filepath) as fd:
+    version_file_data = fd.read()
 
-    patterns = r"""#\s*define\s+ZSTD_VERSION_MAJOR\s+([0-9]+)
+  patterns = r"""#\s*define\s+ZSTD_VERSION_MAJOR\s+([0-9]+)
 #\s*define\s+ZSTD_VERSION_MINOR\s+([0-9]+)
 #\s*define\s+ZSTD_VERSION_RELEASE\s+([0-9]+)
 """
-    regex = re.compile(patterns, re.MULTILINE)
-    version_match = regex.search(version_file_data)
-    if version_match:
-        return version_match.groups()
-    raise RuntimeError("Unable to find version string.")
+  regex = re.compile(patterns, re.MULTILINE)
+  version_match = regex.search(version_file_data)
+  if version_match:
+    return version_match.groups()
+  raise RuntimeError("Unable to find version string.")
 
 
 def main():
-    if len(sys.argv) < 2:
-        usage()
+  if len(sys.argv) < 2:
+    usage()
 
-    filepath = sys.argv[1]
-    version_tup = find_version(filepath)
-    print('.'.join(version_tup))
+  filepath = sys.argv[1]
+  version_tup = find_version(filepath)
+  print('.'.join(version_tup))
 
 
 if __name__ == '__main__':
-    main()
+  main()

8e61ac816133396b91ab17b327d76ae5356ca653
W. Felix Handte <w@felixhandte.com>
2018-12-19 12:36:10 -0800

    Use Unused Variable in ERR_getErrorString()

--- lib/common/error_private.c
+++ lib/common/error_private.c
@@ -15,6 +15,7 @@
 const char* ERR_getErrorString(ERR_enum code)
 {
 #ifdef ZSTD_STRIP_ERROR_STRINGS
+    (void)code;
     return "Error strings stripped";
 #else
     static const char* const notErrorCode = "Unspecified error code";

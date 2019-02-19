0c86d2346781c4ec21042087dfe86c78dcce51af
Nick Terrell <terrelln@fb.com>
2019-02-19 15:49:48 -0800

    [Windows] Move public headers to include/

--- lib/dll/example/build_package.bat
+++ lib/dll/example/build_package.bat
@@ -6,14 +6,15 @@ COPY programs\datagen.h bin\example\
 COPY programs\util.h bin\example\
 COPY programs\platform.h bin\example\
 COPY lib\common\mem.h bin\example\
-COPY lib\common\zstd_errors.h bin\example\
 COPY lib\common\zstd_internal.h bin\example\
 COPY lib\common\error_private.h bin\example\
 COPY lib\common\xxhash.h bin\example\
-COPY lib\zstd.h bin\include\
 COPY lib\libzstd.a bin\static\libzstd_static.lib
 COPY lib\dll\libzstd.* bin\dll\
 COPY lib\dll\example\Makefile bin\example\
 COPY lib\dll\example\fullbench-dll.* bin\example\
 COPY lib\dll\example\README.md bin\
+COPY lib\zstd.h bin\include\
+COPY lib\common\zstd_errors.h bin\include\
+COPY lib\dictBuilder\zdict.h bin\include\
 COPY programs\zstd.exe bin\zstd.exe

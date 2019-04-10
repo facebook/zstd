7b424f81ec903227136f488e25b680c95ba4fd31
Yann Collet <cyan@fb.com>
2019-04-10 15:22:18 -0700

    FreeBSD_11 specific fix
    C11 mandates the definition of timespec_get() and TIME_UTC.
    However, FreeBSD11 announce C11 compliance, but does not provifr timespec_get(),
    breaking link stage for benchfn.
    Since it does not provide TIME_UTC either, which is also required by C11,
    test this macro: this will automatically rule out FreeBSD 11 for this code path
    (it will use the backup C90 path instead, based on clock_t).
    
    The issue seeems fixed in FreeBSD 12.

--- programs/timefn.c
+++ programs/timefn.c
@@ -83,7 +83,8 @@ PTime UTIL_getSpanTimeNano(UTIL_time_t clockStart, UTIL_time_t clockEnd)
 
 
 
-#elif (defined (__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L) /* C11 */)
+#elif (defined (__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L) /* C11 */) \
+    && defined(TIME_UTC) /* C11 requires timespec_get, but FreeBSD 11 lacks it, while still claiming C11 compliance */
 
 #include <stdlib.h>   /* abort */
 #include <stdio.h>    /* perror */

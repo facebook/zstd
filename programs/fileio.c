43d463eccb329c486a73447805536561d8680d27
Ephraim Park <ephiepark@fb.com>
2019-06-03 17:31:14 -0700

    zstdcli : moving start time and cpu initialization after potential prompt

--- programs/fileio.c
+++ programs/fileio.c
@@ -1236,9 +1236,6 @@ static int FIO_compressFilename_dstFile(FIO_prefs_t* const prefs,
                                         const char* srcFileName,
                                         int compressionLevel)
 {
-    UTIL_time_t const timeStart = UTIL_getTime();
-    clock_t const cpuStart = clock();
-
     int closeDstFile = 0;
     int result;
     stat_t statbuf;
@@ -1261,6 +1258,9 @@ static int FIO_compressFilename_dstFile(FIO_prefs_t* const prefs,
           && UTIL_getFileStat(srcFileName, &statbuf))
             transfer_permissions = 1;
     }
+
+    UTIL_time_t const timeStart = UTIL_getTime();
+    clock_t const cpuStart = clock();
 
     result = FIO_compressFilename_internal(prefs, ress, dstFileName, srcFileName, compressionLevel);
 

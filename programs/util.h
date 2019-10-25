857268b32c8efb99f1f40fab3534850f3871f007
Bimba Shrestha <bimbashrestha@fb.com>
2019-10-25 15:15:28 -0700

    Gating named pipe support in hedaer file

--- programs/util.h
+++ programs/util.h
@@ -135,7 +135,10 @@ U32 UTIL_isDirectory(const char* infilename);
 int UTIL_getFileStat(const char* infilename, stat_t* statbuf);
 int UTIL_isSameFile(const char* file1, const char* file2);
 
+#ifndef _MSC_VER
 U32 UTIL_isFIFO(const char* infilename);
+#endif
+
 U32 UTIL_isLink(const char* infilename);
 #define UTIL_FILESIZE_UNKNOWN  ((U64)(-1))
 U64 UTIL_getFileSize(const char* infilename);

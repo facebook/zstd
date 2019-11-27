82ba5c6d297af9379e0058b96b1ea88ad4a5a2cc
Bimba Shrestha <bimbashrestha@fb.com>
2019-11-27 11:13:44 -0800

    Move check set before block

--- lib/compress/zstd_compress.c
+++ lib/compress/zstd_compress.c
@@ -2857,11 +2857,11 @@ size_t ZSTD_loadCEntropy(ZSTD_compressedBlockState_t* bs, void* workspace,
 {
     const BYTE* dictPtr = (const BYTE*)dict;    /* skip magic num and dict ID */
     const BYTE* const dictEnd = dictPtr + dictSize;
-    unsigned hasZeroWeights = 1;
     dictPtr += 8;
+    bs->entropy.huf.repeatMode = HUF_repeat_check;
 
     {   unsigned maxSymbolValue = 255;
-        bs->entropy.huf.repeatMode = HUF_repeat_check;
+        unsigned hasZeroWeights = 1;
         size_t const hufHeaderSize = HUF_readCTable((HUF_CElt*)bs->entropy.huf.CTable, &maxSymbolValue, dictPtr,
             dictEnd-dictPtr, &hasZeroWeights);
 

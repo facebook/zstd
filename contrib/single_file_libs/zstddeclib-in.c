de009cd45a8b00dedb6dde82c3daaa9dd57685ce
Carl Woffenden <cwoffenden@gmail.com>
2020-04-07 18:17:59 +0200

    Fixed clash when projects are already using xxHash
    Undefing XXH_* macros allows the `.c` to build standalone without clashes. Removing `xxhash.c` and only including the header is the correct usage (according to `XXH_PRIVATE_API`).

--- contrib/single_file_libs/zstddeclib-in.c
+++ contrib/single_file_libs/zstddeclib-in.c
@@ -1,7 +1,7 @@
 /**
  * \file zstddeclib.c
  * Single-file Zstandard decompressor.
- * 
+ *
  * Generate using:
  * \code
  *	combine.sh -r ../../lib -r ../../lib/common -r ../../lib/decompress -o zstddeclib.c zstddeclib-in.c
@@ -18,17 +18,23 @@
  */
 /*
  * Settings to bake for the standalone decompressor.
- * 
+ *
  * Note: It's important that none of these affects 'zstd.h' (only the
  * implementation files we're amalgamating).
- * 
+ *
  * Note: MEM_MODULE stops xxhash redefining BYTE, U16, etc., which are also
  * defined in mem.h (breaking C99 compatibility).
+ *
+ * Note: the undefs for xxHash allow Zstd's implementation to coinside with with
+ * standalone xxHash usage (with global defines).
  */
 #define DEBUGLEVEL 0
 #define MEM_MODULE
+#undef  XXH_NAMESPACE
 #define XXH_NAMESPACE ZSTD_
+#undef  XXH_PRIVATE_API
 #define XXH_PRIVATE_API
+#undef  XXH_INLINE_ALL
 #define XXH_INLINE_ALL
 #define ZSTD_LEGACY_SUPPORT 0
 #define ZSTD_LIB_COMPRESSION 0
@@ -36,15 +42,14 @@
 #define ZSTD_NOBENCH
 #define ZSTD_STRIP_ERROR_STRINGS
 
-/* common */
+/* lib/common */
 #include "debug.c"
 #include "entropy_common.c"
 #include "error_private.c"
 #include "fse_decompress.c"
-#include "xxhash.c"
 #include "zstd_common.c"
 
-/* decompress */
+/* lib/decompress */
 #include "huf_decompress.c"
 #include "zstd_ddict.c"
 #include "zstd_decompress.c"

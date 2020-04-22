133679e492fdc21521200e82adc18445c9cd6919
Bimba Shrestha <bimbashrestha@fb.com>
2020-04-22 10:25:18 -0700

    copy paste error fix

--- contrib/largeNbDicts/largeNbDicts.c
+++ contrib/largeNbDicts/largeNbDicts.c
@@ -415,7 +415,7 @@ static void freeCDictCollection(cdict_collection_t cdictc)
 /* returns .buffers=NULL if operation fails */
 static cdict_collection_t createCDictCollection(const void* dictBuffer, size_t dictSize, size_t nbCDict, int cLevel)
 {
-    ZSTD_CDict** const cdicts = malloc(nbCDict * sizeof(ZSTD_DDict*));
+    ZSTD_CDict** const cdicts = malloc(nbCDict * sizeof(ZSTD_CDict*));
     assert(cdicts != NULL);
     if (cdicts==NULL) return kNullCDictCollection;
     for (size_t dictNb=0; dictNb < nbCDict; dictNb++) {

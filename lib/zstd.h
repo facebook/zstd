1b6d1b9ac733bb775e0944507fc66477809f9705
Bimba Shrestha <bimbashrestha@fb.com>
2020-06-11 22:42:11 -0700

    making cctxParams a pointer

--- lib/zstd.h
+++ lib/zstd.h
@@ -1403,7 +1403,7 @@ ZSTDLIB_API ZSTD_CDict* ZSTD_createCDict_advanced(const void* dict, size_t dictS
 ZSTDLIB_API ZSTD_CDict* ZSTD_createCDict_advanced2(const void* dict, size_t dictSize,
                                                   ZSTD_dictLoadMethod_e dictLoadMethod,
                                                   ZSTD_dictContentType_e dictContentType,
-                                                  ZSTD_CCtx_params cctxParams,
+                                                  ZSTD_CCtx_params* cctxParams,
                                                   ZSTD_customMem customMem);
 
 ZSTDLIB_API ZSTD_DDict* ZSTD_createDDict_advanced(const void* dict, size_t dictSize,

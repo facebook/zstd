b93aeae3d8a1b93684f10bc1fc37581c42ffc04d
Niadb <34917535+Niadb@users.noreply.github.com>
2020-07-28 01:20:36 -0600

    Update compiler.h

--- lib/common/compiler.h
+++ lib/common/compiler.h
@@ -192,13 +192,15 @@
 #  endif
 #endif
 
+#ifndef STATIC_BMI2
+    #define STATIC_BMI2 0
+#endif
+
 /* If STATIC_BMI2 was enabled force DYNAMIC_BMI2 on */
 #if STATIC_BMI2 == 1
 #   ifndef DYNAMIC_BMI2
 #       define DYNAMIC_BMI2 1 //Build it statically targeting BMI2, so enable DYNAMIC_BMI2 if it wasn't already on
 #   endif
 #else
-    #define STATIC_BMI2 0
-#endif
 
 #endif /* ZSTD_COMPILER_H */

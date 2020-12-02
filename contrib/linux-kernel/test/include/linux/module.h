87b38156be8f7f4681db19920c7d13f01b9ce84e
Nick Terrell <terrelln@fb.com>
2020-12-02 01:11:41 -0800

    [contrib][linux-kernel] Fix test warnings

--- contrib/linux-kernel/test/include/linux/module.h
+++ contrib/linux-kernel/test/include/linux/module.h
@@ -12,8 +12,7 @@
 
 #define EXPORT_SYMBOL(symbol)                                                  \
   void* __##symbol = symbol
-#define MODULE_LICENSE(license) static char const *const LICENSE = license
-#define MODULE_DESCRIPTION(description)                                        \
-  static char const *const DESCRIPTION = description
+#define MODULE_LICENSE(license)
+#define MODULE_DESCRIPTION(description)
 
 #endif

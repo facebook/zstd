713d4953f712ddc7477877549a0eaebcd0062714
Yann Collet <yann.collet.73@gmail.com>
2021-03-03 18:00:41 -0800

    fixed gcc-7 conversion warning

--- contrib/seekable_format/zstdseek_compress.c
+++ contrib/seekable_format/zstdseek_compress.c
@@ -71,7 +71,7 @@ size_t ZSTD_seekable_frameLog_allocVec(ZSTD_frameLog* fl)
     fl->entries = (framelogEntry_t*)malloc(
             sizeof(framelogEntry_t) * FRAMELOG_STARTING_CAPACITY);
     if (fl->entries == NULL) return ERROR(memory_allocation);
-    fl->capacity = FRAMELOG_STARTING_CAPACITY;
+    fl->capacity = (U32)FRAMELOG_STARTING_CAPACITY;
 
     return 0;
 }

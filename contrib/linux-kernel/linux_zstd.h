de9de869a39511683a86c2a3e6a7bdb898523cb3
Nick Terrell <terrelln@fb.com>
2021-03-29 14:23:36 -0700

    [copyright][license] Switch to yearless copyright and some cleanup in the linux-kernel files
    * Switch to yearless copyright per FB policy
    * Fix up SPDX-License-Identifier lines in `contrib/linux-kernel` sources
    * Add zstd copyright/license header to the `contrib/linux-kernel` sources
    * Update the `tests/test-license.py` to check for yearless copyright
    * Improvements to `tests/test-license.py`
    * Check `contrib/linux-kernel` in `tests/test-license.py`

--- contrib/linux-kernel/linux_zstd.h
+++ contrib/linux-kernel/linux_zstd.h
@@ -1,18 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2016-present, Yann Collet, Facebook, Inc.
+ * Copyright (c) Yann Collet, Facebook, Inc.
  * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of https://github.com/facebook/zstd.
- * An additional grant of patent rights can be found in the PATENTS file in the
- * same directory.
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License version 2 as published by the
- * Free Software Foundation. This program is dual-licensed; you may select
- * either version 2 of the GNU General Public License ("GPL") or BSD license
- * ("BSD").
+ * This source code is licensed under both the BSD-style license (found in the
+ * LICENSE file in the root directory of https://github.com/facebook/zstd) and
+ * the GPLv2 (found in the COPYING file in the root directory of
+ * https://github.com/facebook/zstd). You may select, at your option, one of the
+ * above-listed licenses.
  */
 
 #ifndef LINUX_ZSTD_H

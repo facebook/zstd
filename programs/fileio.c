4f9c6fdb7f3f2482cb2db52b1867d051f8bf1c0f
W. Felix Handte <w@felixhandte.com>
2021-05-05 13:13:56 -0400

    Attempt to Fix Windows Build Error

--- programs/fileio.c
+++ programs/fileio.c
@@ -702,14 +702,19 @@ FIO_openDstFile(FIO_ctx_t* fCtx, FIO_prefs_t* const prefs,
         /* Windows requires opening the file as a "binary" file to avoid
          * mangling. This macro doesn't exist on unix. */
         const int openflags = O_WRONLY|O_CREAT|O_TRUNC|O_BINARY;
+        const int fd = _open(dstFileName, openflags, mode);
+        FILE* f = NULL;
+        if (fd != -1) {
+            f = _fdopen(fd, "wb");
+        }
 #else
         const int openflags = O_WRONLY|O_CREAT|O_TRUNC;
-#endif
         const int fd = open(dstFileName, openflags, mode);
         FILE* f = NULL;
         if (fd != -1) {
             f = fdopen(fd, "wb");
         }
+#endif
         if (f == NULL) {
             DISPLAYLEVEL(1, "zstd: %s: %s\n", dstFileName, strerror(errno));
         }

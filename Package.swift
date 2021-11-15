81b96205afb5b70dcbf292d48e07c0c66945285b
Lvv.me <cntrump@gmail.com>
2021-11-15 13:23:50 +0800

    Using `module.modulemap` replace symbol link for public header

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "zstd",
-            targets: [ "zstd" ])
+            name: "libzstd",
+            targets: [ "libzstd" ])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "zstd",
+            name: "libzstd",
             path: "lib",
             sources: [ "common", "compress", "decompress", "dictBuilder" ],
-            publicHeadersPath: "include",
+            publicHeadersPath: "modulemap",
             cSettings: [
                 .headerSearchPath(".")
             ])

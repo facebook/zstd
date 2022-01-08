d1325174e09c38b5ab0125ca09a1353a61eb71c7
Hideki Yamane <henrich@debian.org>
2022-01-08 23:07:26 +0900

    Fix build failure in Alpha architecture
    Taken from debian/patches
    See https://bugs.debian.org/962676

--- programs/util.c
+++ programs/util.c
@@ -167,7 +167,7 @@ int UTIL_utime(const char* filename, const stat_t *statbuf)
      * that struct stat has a struct timespec st_mtim member. We need this
      * check because there are some platforms that claim to be POSIX 2008
      * compliant but which do not have st_mtim... */
-#if (PLATFORM_POSIX_VERSION >= 200809L) && defined(st_mtime)
+#if (PLATFORM_POSIX_VERSION >= 200809L) && defined(st_mtime) || defined(__alpha__)
     /* (atime, mtime) */
     struct timespec timebuf[2] = { {0, UTIME_NOW} };
     timebuf[1] = statbuf->st_mtim;

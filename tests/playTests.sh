501a353b9154e6885031fe14e2ded5760c97f854
brailovich <91924341+brailovich@users.noreply.github.com>
2022-01-26 18:56:52 -0800

    Update playTests.sh

--- tests/playTests.sh
+++ tests/playTests.sh
@@ -393,7 +393,7 @@ zstd -c -r < tmp > tmp.zst
 # combination of -r with empty folder
 mkdir -p tmpEmptyDir
 zstd -r tmpEmptyDir 2>tmplog2
-if [grep "aborting" tmplog2]; then
+if [ grep "aborting" tmplog2 ]; then
   println "Should not abort on empty directory"
   rm -rf tmplog2
   rm -rf tmpEmptyDir

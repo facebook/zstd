7a3997c21a6e7620eaf6562b69326e23ffdde23d
Dirk Müller <dirk@dmllr.de>
2022-03-10 00:25:05 +0100

    Handle newer less versions in zstdless testing
    Newer less versions appear to have changed how stderr
    and stdout are showing error messages. hardcode the
    expected behavior to make the tests pass with any less version.
    
    Also set locale to C so that the strings are matching.

--- tests/cli-tests/cltools/zstdless.sh
+++ tests/cli-tests/cltools/zstdless.sh
@@ -7,4 +7,4 @@ zstdless file.zst
 println "+ pass parameters"
 zstdless -N file.zst # This parameter does not produce line #s when piped, but still serves to test that the flag went to less and not zstd
 println "+ bad path"
-zstdless bad.zst
+zstdless bad.zst >&2

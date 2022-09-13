e46b12e1b46554ddf8711c5c60ae3aa461c214aa
Danielle Rozenblit <drozenblit@fb.com>
2022-09-12 18:56:59 -0700

    fix indentation

--- lib/decompress/zstd_decompress.c
+++ lib/decompress/zstd_decompress.c
@@ -2178,23 +2178,23 @@ size_t ZSTD_decompressStream(ZSTD_DStream* zds, ZSTD_outBuffer* output, ZSTD_inB
             }
         case zdss_flush:
             {
-            size_t const toFlushSize = zds->outEnd - zds->outStart;
-            size_t const flushedSize = ZSTD_limitCopy(op, (size_t)(oend-op), zds->outBuff + zds->outStart, toFlushSize);
+                size_t const toFlushSize = zds->outEnd - zds->outStart;
+                size_t const flushedSize = ZSTD_limitCopy(op, (size_t)(oend-op), zds->outBuff + zds->outStart, toFlushSize);
 
-            op = op ? op + flushedSize : op;
+                op = op ? op + flushedSize : op;
 
-            zds->outStart += flushedSize;
-            if (flushedSize == toFlushSize) {  /* flush completed */
-                zds->streamStage = zdss_read;
-                if ( (zds->outBuffSize < zds->fParams.frameContentSize)
-                    && (zds->outStart + zds->fParams.blockSizeMax > zds->outBuffSize) ) {
-                    DEBUGLOG(5, "restart filling outBuff from beginning (left:%i, needed:%u)",
-                            (int)(zds->outBuffSize - zds->outStart),
-                            (U32)zds->fParams.blockSizeMax);
-                    zds->outStart = zds->outEnd = 0;
-                }
-                break;
-            } }
+                zds->outStart += flushedSize;
+                if (flushedSize == toFlushSize) {  /* flush completed */
+                    zds->streamStage = zdss_read;
+                    if ( (zds->outBuffSize < zds->fParams.frameContentSize)
+                        && (zds->outStart + zds->fParams.blockSizeMax > zds->outBuffSize) ) {
+                        DEBUGLOG(5, "restart filling outBuff from beginning (left:%i, needed:%u)",
+                                (int)(zds->outBuffSize - zds->outStart),
+                                (U32)zds->fParams.blockSizeMax);
+                        zds->outStart = zds->outEnd = 0;
+                    }
+                    break;
+            }   }
             /* cannot complete flush */
             someMoreWork = 0;
             break;

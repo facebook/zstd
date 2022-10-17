b34729018cd4a38d8e3efdc604d30ec9c3081f24
Danielle Rozenblit <drozenblit@fb.com>
2022-10-17 07:55:07 -0700

    Minor simplication: no longer need to check src size if using cardinality for minTableLog

--- lib/compress/huf_compress.c
+++ lib/compress/huf_compress.c
@@ -1249,11 +1249,9 @@ unsigned HUF_cardinality(const unsigned* count, unsigned maxSymbolValue)
 
 unsigned HUF_minTableLog(size_t srcSize, unsigned symbolCardinality)
 {
-    U32 minBitsSrc = ZSTD_highbit32((U32)(srcSize)) + 1;
     U32 minBitsSymbols = ZSTD_highbit32(symbolCardinality) + 1;
-    U32 minBits = minBitsSrc < minBitsSymbols ? minBitsSrc : minBitsSymbols;
     assert(srcSize > 1); /* Not supported, RLE should be used instead */
-    return minBits;
+    return minBitsSymbols;
 }
 
 unsigned HUF_optimalTableLog(unsigned maxTableLog, size_t srcSize, unsigned maxSymbolValue, void* workSpace, size_t wkspSize, HUF_CElt* table, const unsigned* count, HUF_depth_mode depthMode)

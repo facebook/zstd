5f7ec8cf0964250f19aa5553daeace7ed095a7d2
Mugu~~ <mugunthan.selvanayagam@multicorewareinc.com>
2025-07-01 12:08:01 +0530

    CI: Update build_package.bat for CMake builds

--- lib/dll/example/build_package.bat
+++ lib/dll/example/build_package.bat
@@ -1,10 +1,16 @@
 @echo off
 setlocal
 
-rem Create required directories.
+rem Detect build type based on available files
+set BUILD_TYPE=make
+if exist "build\cmake\build\lib\Release\zstd_static.lib" set BUILD_TYPE=cmake
+
+echo Detected build type: %BUILD_TYPE%
+
+rem Create required directories
 mkdir bin\dll bin\static bin\example bin\include
 
-rem Copy files using a subroutine. Exits immediately on failure.
+rem Copy common files using a subroutine. Exits immediately on failure.
 call :copyFile "tests\fullbench.c" "bin\example\"
 call :copyFile "programs\datagen.c" "bin\example\"
 call :copyFile "programs\datagen.h" "bin\example\"
@@ -14,16 +20,29 @@ call :copyFile "lib\common\mem.h" "bin\example\"
 call :copyFile "lib\common\zstd_internal.h" "bin\example\"
 call :copyFile "lib\common\error_private.h" "bin\example\"
 call :copyFile "lib\common\xxhash.h" "bin\example\"
-call :copyFile "lib\libzstd.a" "bin\static\libzstd_static.lib"
-call :copyFile "lib\dll\libzstd.*" "bin\dll\"
 call :copyFile "lib\dll\example\Makefile" "bin\example\"
 call :copyFile "lib\dll\example\fullbench-dll.*" "bin\example\"
-call :copyFile "lib\dll\example\README.md" "bin\"
 call :copyFile "lib\zstd.h" "bin\include\"
 call :copyFile "lib\zstd_errors.h" "bin\include\"
 call :copyFile "lib\zdict.h" "bin\include\"
-call :copyFile "programs\zstd.exe" "bin\zstd.exe"
 
+rem Copy build-specific files
+if "%BUILD_TYPE%"=="cmake" (
+    echo Copying CMake build artifacts...
+    call :copyFile "build\cmake\build\lib\Release\zstd_static.lib" "bin\static\libzstd_static.lib"
+    call :copyFile "build\cmake\build\lib\Release\zstd.dll" "bin\dll\libzstd.dll"
+    call :copyFile "build\cmake\build\lib\Release\zstd.lib" "bin\dll\zstd.lib"
+    call :copyFile "build\cmake\build\programs\Release\zstd.exe" "bin\zstd.exe"
+    call :copyFile "lib\dll\example\README.md" "bin\README.md"
+) else (
+    echo Copying Make build artifacts...
+    call :copyFile "lib\libzstd.a" "bin\static\libzstd_static.lib"
+    call :copyFile "lib\dll\libzstd.*" "bin\dll\"
+    call :copyFile "programs\zstd.exe" "bin\zstd.exe"
+    call :copyFile "lib\dll\example\README.md" "bin\"
+)
+
+echo Build package created successfully for %BUILD_TYPE% build!
 endlocal
 exit /b 0
 

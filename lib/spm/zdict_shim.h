cdca62a64e748880a5a489cadfbb863853b02f86
Lukas Kollmer <hey@lukaskollmer.de>
2026-02-02 13:24:01 +0100

    SPM: ZSTDLIB_STATIC_API

---
+++ lib/spm/zdict_shim.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under both the BSD-style license (found in the
+ * LICENSE file in the root directory of this source tree) and the GPLv2 (found
+ * in the COPYING file in the root directory of this source tree).
+ * You may select, at your option, one of the above-listed licenses.
+ */
+
+#ifndef ZSTD_ZDICT_SHIM_H
+#define ZSTD_ZDICT_SHIM_H
+
+#define ZSTD_STATIC_LINKING_ONLY
+#include "../zdict.h"
+
+#endif /* ZSTD_SHIM_H */

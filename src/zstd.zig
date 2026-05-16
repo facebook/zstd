92127e8294e6fd352450dd4a3e3f4ed4048b8874
Muhammad Fiaz <contact@muhammadfiaz.com>
2026-05-16 13:14:04 +0530

    Update zstd.zig

--- src/zstd.zig
+++ src/zstd.zig
@@ -1,9 +1,16 @@
-pub const c = @cImport({
+const builtin = @import("builtin");
+const is_aarch64_macos = builtin.target.cpu.arch == .aarch64 and builtin.target.os.tag == .macos;
+
+pub const c = if (is_aarch64_macos) @cImport({
+    @cDefine("ZSTD_NO_INTRINSICS", "");
+    @cDefine("ZSTD_DISABLE_ASM", "");
     @cDefine("ZSTD_STATIC_LINKING_ONLY", "");
-    @cDefine("ZSTD_LEGACY_SUPPORT", "1");
     @cInclude("zstd.h");
-    @cInclude("zdict.h");
     @cInclude("zstd_errors.h");
-    @cInclude("zbuff.h");
-    @cInclude("zstd_legacy.h");
+    @cInclude("zdict.h");
+}) else @cImport({
+    @cDefine("ZSTD_STATIC_LINKING_ONLY", "");
+    @cInclude("zstd.h");
+    @cInclude("zstd_errors.h");
+    @cInclude("zdict.h");
 });

28679e2992ba8f9b154d478c07bd1a053bf10c01
Muhammad Fiaz <contact@muhammadfiaz.com>
2026-07-28 20:27:59 +0530

    Update zstd.zig

--- src/zstd.zig
+++ src/zstd.zig
@@ -1,16 +1,11 @@
 const builtin = @import("builtin");
-const is_aarch64_macos = builtin.target.cpu.arch == .aarch64 and builtin.target.os.tag == .macos;
 
-pub const c = if (is_aarch64_macos) @cImport({
-    @cDefine("ZSTD_NO_INTRINSICS", "");
-    @cDefine("ZSTD_DISABLE_ASM", "");
-    @cDefine("ZSTD_STATIC_LINKING_ONLY", "");
-    @cInclude("zstd.h");
-    @cInclude("zstd_errors.h");
-    @cInclude("zdict.h");
-}) else @cImport({
-    @cDefine("ZSTD_STATIC_LINKING_ONLY", "");
-    @cInclude("zstd.h");
-    @cInclude("zstd_errors.h");
-    @cInclude("zdict.h");
-});
+pub const c = struct {
+    pub extern fn ZSTD_compress(dst: ?*anyopaque, dstCapacity: usize, src: ?*const anyopaque, srcSize: usize, compressionLevel: c_int) usize;
+    pub extern fn ZSTD_decompress(dst: ?*anyopaque, dstCapacity: usize, src: ?*const anyopaque, compressedSize: usize) usize;
+    pub extern fn ZSTD_getFrameContentSize(src: ?*const anyopaque, srcSize: usize) u64;
+    pub extern fn ZSTD_isError(code: usize) c_uint;
+    pub extern fn ZSTD_compressBound(srcSize: usize) usize;
+    pub extern fn ZSTD_getErrorName(code: usize) [*:0]const u8;
+};
+

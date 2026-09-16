2352104d11a9ba81dddbae9ae312a2bd322f1696
Yongqiang Tian <yqtian668@gmail.com>
2026-09-16 03:04:04 +0000

    headers: fix GCC version checks for deprecation messages
    Use GCC predefined macro spellings so pre-C++14 users receive the replacement API text carried by the deprecation attributes.
    
    Signed-off-by: Yongqiang Tian <yqtian668@gmail.com>

--- lib/zstd.h
+++ lib/zstd.h
@@ -62,7 +62,7 @@ extern "C" {
 #else
 #  if defined (__cplusplus) && (__cplusplus >= 201402) /* C++14 or greater */
 #    define ZSTD_DEPRECATED(message) [[deprecated(message)]]
-#  elif (defined(GNUC) && (GNUC > 4 || (GNUC == 4 && GNUC_MINOR >= 5))) || defined(__clang__) || defined(__IAR_SYSTEMS_ICC__)
+#  elif (defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5))) || defined(__clang__) || defined(__IAR_SYSTEMS_ICC__)
 #    define ZSTD_DEPRECATED(message) __attribute__((deprecated(message)))
 #  elif defined(__GNUC__) && (__GNUC__ >= 3)
 #    define ZSTD_DEPRECATED(message) __attribute__((deprecated))
